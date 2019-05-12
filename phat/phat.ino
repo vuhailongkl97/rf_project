94e14b0fd1a7304619873b20898427f2de94a80d
vu hai long <vuhailongkl97@gmail.com>
2019-05-12 13:05:20 +0700

    update project with bidirection data tranfer/receiver

---
+++ phat/phat.ino
@@ -0,0 +1,85 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Nokia5110.h>
+
+
+/*----- Declare all constant pin to be used ----*/
+
+#define CE_PIN   9
+#define CSN_PIN 10
+#define JOYSTICK_X A0
+#define JOYSTICK_Y A1
+
+
+Nokia5110 lcd(8, 4, 5, 6,7 ); // (PIN_SCE, PIN_RESET, PIN_DC, PIN_SDIN, PIN_SCLK)
+// PIN_SDIN and PIN_SCLK should be driven by PWM enabled pins
+
+unsigned char buff[100];
+
+const uint64_t pipe = 0xE8E8F0F0E1LL; // This is the transmit pipe to communicate the two module
+
+
+/*-----Object Declaration ----*/
+
+RF24 radio(CE_PIN, CSN_PIN); // Activate  the Radio
+
+/*-----Declaration of Variables -----*/
+
+int joystick[2];  // Two element array holding the Joystick readings
+
+void setup()   
+{
+  lcd.init(); 
+  Serial.begin(9600); /* Opening the Serial Communication */
+  radio.begin();
+  radio.openWritingPipe(pipe);
+  radio.openReadingPipe(1,pipe);
+   radio.startListening();
+}//--(end setup )---
+
+char directions =11;
+char power ;
+enum m_dir{GO_A_HEAD , LEFT , BACK, RIGHT};
+char i;
+
+
+void loop()   /* Runs Continuously */
+{
+   radio.stopListening();
+   delay(50);
+ for(i = 0;i < 5; i++) {
+   joystick[0] = analogRead(JOYSTICK_X); 
+    joystick[1] = analogRead(JOYSTICK_Y); 
+  if ( joystick[0] > 700 && joystick[1] > 300 ) 
+        directions = GO_A_HEAD;
+      else if ( joystick[0] > 300 && joystick[1] < 10 )
+        directions = LEFT;
+      else if ( joystick[0] < 10 && joystick[1] > 300 )
+        directions = BACK;
+      else if ( joystick[0] > 300 && joystick[1] > 700 )
+        directions = RIGHT;
+  radio.write(&directions, sizeof(directions) );
+ }
+    radio.startListening();
+    delay(50);
+  if(radio.available())
+  {
+      bool done = false;
+    while (!done)
+    {
+      // Fetching the data payload
+     
+      done = radio.read( &power, sizeof(power) );
+      //Serial.println(int(power) );
+       //lcd.clear();
+      lcd.gotoXY(6, 2); 
+       sprintf(buff,"POWER %d %%",power);
+        lcd.string(buff);
+    }
+  }
+
+
+
+
+}
---
+++ thu/thu.ino
@@ -0,0 +1,143 @@
+/*----- Import all required Libraries -----*/
+ 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+ 
+/*----- Declare all constant pin to be used ----*/
+ 
+#define CE_PIN   9
+#define CSN_PIN 10
+ 
+const uint64_t pipe = 0xE8E8F0F0E1LL; // This is the transmit pipe to communicate the two module
+
+ 
+/*-----Object Declaration ----*/
+ 
+RF24 radio(CE_PIN, CSN_PIN); // Activate the Radio
+ 
+/*-----Declaration of Variables -----*/
+ 
+int joystick[2];  //  Two element array holding the Joystick readings
+ 
+#define in1 2   // A+
+#define in2 3   // A-
+#define in3 4// B-
+#define in4 5  // B+
+#define ena A0
+#define enb A1
+#define POWER_PIN A3
+
+
+int init_speed=80;
+int max_speed = 120;
+
+/*
+ * cac ham co ban tien lui , dung , quay trai , phai 
+ * su dung de test khi khong dung cac he so PD
+*/
+void lui(unsigned long ts){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);  
+  digitalWrite(in3, HIGH);  
+  digitalWrite(in4, LOW);  
+  analogWrite(ena, init_speed); 
+  analogWrite(enb, init_speed); 
+  delay(ts);
+}
+
+void tien(unsigned long ts){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);  
+  digitalWrite(in3, LOW);  
+  digitalWrite(in4, HIGH); 
+  analogWrite(ena, init_speed); 
+  analogWrite(enb, init_speed);  
+  delay(ts);
+}
+
+void retrai(unsigned long t){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);  
+  digitalWrite(in3, LOW);  
+  digitalWrite(in4, HIGH);  
+  analogWrite(ena, init_speed); 
+  analogWrite(enb, init_speed); 
+  delay(t);
+}
+
+void rephai(unsigned long ts){
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);  
+  digitalWrite(in3, HIGH);  
+  digitalWrite(in4, LOW);  
+  analogWrite(ena, init_speed); 
+  analogWrite(enb, init_speed); 
+  delay(ts);
+}
+/*
+  xac dinh gia tri doc duoc tren cac cam bien 
+*/
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(in1, OUTPUT);  
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(POWER_PIN, INPUT);
+
+  pinMode(ena, OUTPUT);
+  pinMode(enb, OUTPUT); 
+  digitalWrite(A4, LOW);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);  
+  digitalWrite(in3, LOW);  
+  digitalWrite(in4, HIGH);  
+
+  Serial.begin(9600); /* Opening the Serial Communication */
+  delay(1000);
+  Serial.println("Nrf24L01 Receiver Starting");
+  radio.begin();
+  radio.openReadingPipe(1,pipe);
+   radio.openWritingPipe(pipe);
+  radio.startListening();
+}
+int num = 0;
+char pin_power;
+char directions = 0;
+enum m_dir{GO_A_HEAD , LEFT , BACK, RIGHT};
+void loop(){
+ radio.startListening();
+ delay(50);
+    if(radio.available()){
+          
+    // Reading the data payload until the RX received everything
+    bool done = false;
+    while (!done)
+    {
+      // Fetching the data payload
+      done = radio.read( &directions, sizeof(directions) );
+      if(directions == GO_A_HEAD) {
+        Serial.println("go a head");
+      }
+      else if(directions == LEFT) {
+        Serial.println("LEFT");
+      }
+      else if(directions == RIGHT) {
+        Serial.println("RIGHT");
+      }
+      else if(directions == BACK) {
+        Serial.println("BACK");
+      }
+    }
+    }
+    radio.stopListening();
+    num = analogRead(POWER_PIN);
+    pin_power = (char)(num/10.2);
+        for(int i = 0; i< 2; i++){
+         radio.write(&pin_power, sizeof(pin_power));
+   
+    }
+  }
